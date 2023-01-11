a62ed48744809a73c45b5eeef345443b03d35509
ShaikhAnis30 <109184307+ShaikhAnis30@users.noreply.github.com>
2023-01-11 13:33:18 +0530

    Method Overriding Completed

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,27 @@
 package com.driver;
 
 public class Main {
-  
+  public static void main(String args[]) {
+    B obj = new B();
+    obj.meth(); // called before Overriding
+    // o/p : Invoking method from class A
+    
+    obj.meth(); // called after Overriding
+    // o/p : Method is overridden in Extendend class B
+  }
+}
+
+class A{
+  public String meth(){
+    String s = "Invoking method from class A";
+    return s;
+  }
+}
+
+class B extends A{
+  @Override
+  public String meth() {
+    String s = "Method is overridden in Extendend class B";
+    return s;
+  }
 }
