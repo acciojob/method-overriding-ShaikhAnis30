5eec6fed025453f2949a5584db6e4b68b1190862
ShaikhAnis30 <anisshaikh8535@gmail.com>
2023-01-13 11:20:27 +0530

    Update 4

---
+++ src/main/java/com/driver/B.java
@@ -0,0 +1,7 @@
+package com.driver;
+
+public class B extends A{  // task 2
+    public String meth(){    // task 4
+        return "Invoking method from class A";
+    }
+}
--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -7,22 +7,9 @@ public class Main {
     obj.meth();
 
 //    System.out.println(obj.meth());  // task 5
-    obj.meth();
+    obj.meth(); // task 5
   }
 }
 
-class A{
-
-  public String meth(){      // task 1
-    return "Invoking method from class A";
-  }
-}
-
-class B extends A{  // task 2
-
-  public String meth(){   // task 4
-    return "Method is overridden in Extended class B";
-  }
-}
 
 
