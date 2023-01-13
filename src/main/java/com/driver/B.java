d30ff6e80515b2dcf7a34c0d28e77579331ce03d
ShaikhAnis30 <anisshaikh8535@gmail.com>
2023-01-13 13:16:55 +0530

    Update 5

--- src/main/java/com/driver/B.java
+++ src/main/java/com/driver/B.java
@@ -1,7 +1,8 @@
 package com.driver;
 
 public class B extends A{  // task 2
+    @Override
     public String meth(){    // task 4
-        return "Invoking method from class A";
+        return "Method is overridden in Extendend class B";
     }
 }
